89975d51fe042be35e17ca5f589e5b3ad1e1f9c9
181404010226 <108257182+181404010226@users.noreply.github.com>
2025-10-20 09:29:05 +0800

    上传基本资源

---
+++ assets/scripts/minibattle/test.ts
@@ -0,0 +1,23 @@
+import { _decorator, Component, instantiate, Node, Prefab } from 'cc';
+import { levelData, LevelData } from './LevelData';
+import { Exmini1_UserData } from './Exmini1_UserData';
+const { ccclass, property } = _decorator;
+
+@ccclass('test')
+export class test extends Component {
+
+    openMiniGame()
+    {
+        
+    }
+
+    onEditDidEnded(editbox, customEventData) {
+        
+    }
+
+    clearData() {
+        
+    }
+}
+
+
